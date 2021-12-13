9318618cd0df0f8e798d3c0b4a026661eebf6485
ThomasButtigieg6.2A <mrthomas6009@gmail.com>
2021-12-13 13:16:58 +0100

    Sprite Downloading for Task 6
    Created a script that successfully connects to the Firebase Storage component and downloads both uploaded sprites

--- Assets/Scripts/Task6Manager.cs
+++ Assets/Scripts/Task6Manager.cs
@@ -1,3 +1,5 @@
+using Firebase.Extensions;
+using Firebase.Storage;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +9,39 @@ public class Task6Manager : MonoBehaviour
     public GameObject squareInst;
     public GameObject circleInst;
 
+    StorageReference storageRef;
+
     void Start()
     {
-        
-    }
+        FirebaseStorage storage = FirebaseStorage.DefaultInstance;
+        StorageReference squareRef = storage.GetReferenceFromUrl("gs://connectedgamingassignment.appspot.com/square.png");
+        StorageReference circleRef = storage.GetReferenceFromUrl("gs://connectedgamingassignment.appspot.com/circle.png");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        const long maxAllowedSize = 1 * 1024 * 1024;
+        squareRef.GetBytesAsync(maxAllowedSize).ContinueWithOnMainThread(task => {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.LogException(task.Exception);
+                // Uh-oh, an error occurred!
+            }
+            else
+            {
+                byte[] fileContents = task.Result;
+                Debug.Log("Finished downloading!");
+            }
+        });
+
+        circleRef.GetBytesAsync(maxAllowedSize).ContinueWithOnMainThread(task => {
+            if (task.IsFaulted || task.IsCanceled)
+            {
+                Debug.LogException(task.Exception);
+                // Uh-oh, an error occurred!
+            }
+            else
+            {
+                byte[] fileContents = task.Result;
+                Debug.Log("Finished downloading!");
+            }
+        });
     }
 }
