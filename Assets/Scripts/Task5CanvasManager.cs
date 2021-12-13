5d89ae5d62e56ac6c929314867eeff55e277ba88
ThomasButtigieg6.2A <mrthomas6009@gmail.com>
2021-12-13 16:16:51 +0100

    Game Logic for Task 5
    Moving the players up and down updates the score appropriately, with the score updating appropriately. Once the score of 10 is hit, the ending screen is displayed

--- Assets/Scripts/Task5CanvasManager.cs
+++ Assets/Scripts/Task5CanvasManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Task5CanvasManager : MonoBehaviour
 {
@@ -12,9 +14,20 @@ public class Task5CanvasManager : MonoBehaviour
     private GameObject winner;
     private GameObject waiting;
 
+    private int player1Score;
+    private int player2Score;
+    private string playerWin;
+
+    [System.NonSerialized]
+    public int currState;
+
+    private const string score1Text = "Player 1 Score: ";
+    private const string score2Text = "Player 2 Score: ";
+
     // Start is called before the first frame update
     void Start()
     {
+        //Gets a reference to all important UI elements
         canv = GameObject.Find("Canvas");
         but1 = canv.transform.Find("MainMenu").Find("Player1But").gameObject;
         but2 = canv.transform.Find("MainMenu").Find("Player2But").gameObject;
@@ -22,11 +35,92 @@ public class Task5CanvasManager : MonoBehaviour
         score1 = canv.transform.Find("GameplayMenu").Find("Player1Score").gameObject;
         score2 = canv.transform.Find("GameplayMenu").Find("Player2Score").gameObject;
         winner = canv.transform.Find("EndMenu").Find("Winner").gameObject;
+
+        //Ensures we start with the main menu
+        ChangeMenu(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Updates the visuals of the score
+        score1.GetComponent<TextMeshProUGUI>().text = score1Text + player1Score.ToString();
+        score2.GetComponent<TextMeshProUGUI>().text = score2Text + player2Score.ToString();
+
+        if (player1Score > 10)
+        {
+            playerWin = "Player 1";
+            ChangeMenu(3);
+        }
+        if (player2Score > 10)
+        {
+            playerWin = "Player 2";
+            ChangeMenu(3);
+        }
+    }
+
+    //Method to change menus
+    private void ChangeMenu(int men)
+    {
+        switch (men)
+        {
+            //Main Menu
+            case 0:
+                canv.transform.Find("MainMenu").gameObject.SetActive(true);
+                canv.transform.Find("GameplayMenu").gameObject.SetActive(false);
+                canv.transform.Find("EndMenu").gameObject.SetActive(false);
+
+                currState = 0;
+                break;
+            //Waiting
+            case 1:
+                canv.transform.Find("MainMenu").gameObject.SetActive(false);
+                canv.transform.Find("GameplayMenu").gameObject.SetActive(true);
+                canv.transform.Find("EndMenu").gameObject.SetActive(false);
+
+                waiting.SetActive(true);
+                score1.SetActive(false);
+                score2.SetActive(false);
+
+                currState = 1;
+                break;
+            //Playing
+            case 2:
+                canv.transform.Find("MainMenu").gameObject.SetActive(false);
+                canv.transform.Find("GameplayMenu").gameObject.SetActive(true);
+                canv.transform.Find("EndMenu").gameObject.SetActive(false);
+
+                waiting.SetActive(false);
+                score1.SetActive(true);
+                score2.SetActive(true);
+
+                currState = 2;
+                break;
+            //Ending
+            case 3:
+                canv.transform.Find("MainMenu").gameObject.SetActive(false);
+                canv.transform.Find("GameplayMenu").gameObject.SetActive(false);
+                canv.transform.Find("EndMenu").gameObject.SetActive(true);
+
+                score1.GetComponent<TextMeshProUGUI>().text = playerWin;
+                currState = 3;
+                break;
+            default:
+                Debug.Log("Menu number doesn't exist");
+                break;
+        }
+    }
+
+    //Method to update the scores
+    public void addScore(bool player)
+    {
+        if (player == false)
+        {
+            player1Score++;
+        }
+        else
+        {
+            player2Score++;
+        }
     }
 }
--- Assets/Scripts/Task5GameManager.cs
+++ Assets/Scripts/Task5GameManager.cs
@@ -7,14 +7,28 @@ public class Task5GameManager : MonoBehaviour
     public GameObject player1;
     public GameObject player2;
 
+    private float player1PivotPos;
+    private float player2PivotPos;
+    private Task5CanvasManager canvManInst;
+
     void Start()
     {
-        
+        canvManInst = gameObject.GetComponent<Task5CanvasManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Calculates distance and increments score when exceeded
+        if (player1.transform.position.y > player1PivotPos + 2 || player1.transform.position.y < player1PivotPos - 2)
+        {
+            canvManInst.addScore(false);
+            player1PivotPos = player1.transform.position.y;
+        }
+        if (player2.transform.position.y > player2PivotPos + 2 || player2.transform.position.y < player2PivotPos - 2)
+        {
+            canvManInst.addScore(true);
+            player2PivotPos = player2.transform.position.y;
+        }
     }
 }
