fda979ea5502f6863181828003840131fdddcfdc
ThomasButtigieg6.2A <mrthomas6009@gmail.com>
2021-12-13 13:02:01 +0100

    Created base scenes
    Created 2 separate scenes to build the tasks separately. Created UI shell for both scenes. Imported TextMeshPro for more elegant UI design.

---
+++ Assets/Scripts/Task6Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task6Manager : MonoBehaviour
+{
+    public GameObject squareInst;
+    public GameObject circleInst;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
